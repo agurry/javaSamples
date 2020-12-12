a5bd3a7e74b1bac54140ad37e2e0225efbd7bdac
agurry <70309370+agurry@users.noreply.github.com>
2020-12-12 10:30:34 -0500

    Add files via upload

---
+++ systemOut.java
@@ -0,0 +1,13 @@
+/* Author: Andrew Gurry
+* Company: Military Integrity Services
+* Date: 2020/12/05
+* Editor: Notepad (No IDE)
+* Purpose: Hello World
+*/
+
+public class systemOut{
+
+public static void main(String[] args){
+	System.out.println("Hello You!");
+	}
+}
